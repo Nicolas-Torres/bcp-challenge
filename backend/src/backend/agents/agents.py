79648c2b600f7acf08c08d6c9aefbf66310603b6
Nicolas Torres <ntorresv.dev@gmail.com>
2026-02-07 18:57:11 -0500

    ✨ feat(backend/agents): Se agrega el context_agent

--- backend/src/backend/agents/agents.py
+++ backend/src/backend/agents/agents.py
@@ -1,5 +1,7 @@
 from backend.tools.policy_rag_tool import tool_consult_policies
 from backend.tools.history_tool import tool_customer_history
+from backend.tools.context_tool import tool_analyze_context
+
 from backend.config import CONFIG
 
 from crewai import Agent
@@ -8,6 +10,19 @@ from langchain_openai import ChatOpenAI
 api_key_open_ai = CONFIG["open-ai-apikey"]
 
 llm = ChatOpenAI(model_name="gpt-4o-mini", api_key=api_key_open_ai)
+
+context_agent = Agent(
+    role='Especialista en Riesgo Situacional',
+    goal='Evaluar el peligro inmediato de la transacción basado en factores externos como ubicación, hora y cuantía.',
+    backstory="""Eres un experto en seguridad operativa bancaria. Tu enfoque es puramente 
+    objetivo: no te distraes con el historial del cliente. Si una transacción ocurre a las 3 AM 
+    en un país de alto riesgo o por un monto excesivo, disparas las alarmas. Tu misión es 
+    identificar si el contexto de la operación facilita un escenario de fraude.""",
+    tools=[tool_analyze_context],
+    llm=llm, # El ChatOpenAI que definiste antes
+    verbose=True,
+    allow_delegation=False
+)
 
 patterns_agent = Agent(
     role='Analista de Comportamiento del Cliente',
