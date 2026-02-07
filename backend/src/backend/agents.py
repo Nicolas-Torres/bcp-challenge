d297fee4162f029fb2173b67adb1ec6e07afda2e
Nicolas Torres <ntorresv.dev@gmail.com>
2026-02-07 18:17:36 -0500

    ✨ feat(backend/agents): Se agrega el agente que revisa el comportamiento del cliente

--- backend/src/backend/agents.py
+++ backend/src/backend/agents.py
@@ -1,4 +1,5 @@
 from backend.rag import tool_consult_policies
+from backend.history import tool_customer_history
 from backend.config import CONFIG
 
 from crewai import Agent
@@ -7,6 +8,19 @@ from langchain_openai import ChatOpenAI
 api_key_open_ai = CONFIG["open-ai-apikey"]
 
 llm = ChatOpenAI(model_name="gpt-4o-mini", api_key=api_key_open_ai)
+
+patterns_agent = Agent(
+    role='Analista de Comportamiento del Cliente',
+    goal='Detectar anomalías comparando la transacción actual con el historial del usuario.',
+    backstory="""Eres un experto en perfiles de consumo. Tu trabajo es responder: 
+        "¿Es normal que este cliente haga esta compra?". 
+        Si el cliente es nuevo, asume riesgo alto por incertidumbre.
+        Si el cliente tiene historial, busca desviaciones en monto (>200% del promedio), 
+        ubicación o dispositivo.""",
+    tools=[tool_customer_history], 
+    llm=llm,
+    verbose=True
+)
 
 policy_agent = Agent(
     role='Fiscal de Seguridad Bancaria',
